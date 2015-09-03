5b69b170502c1e25334d1a45f5b1ecf330b6cb55
Gabriel Poça <gabrielpoca@gmail.com>
2015-09-03 15:43:14 +0100

    Small fixes

--- shipitfile.js
+++ shipitfile.js
@@ -1,32 +1,33 @@
-var path = require('path2/posix');
-
-var options = {
-  default: {
-    workspace: '/tmp/kraken',
-    deployTo: '/apps/kraken',
-    branch: 'v2',
-    repositoryUrl: 'git@github.com:subvisual/kraken.git',
-    keepReleases: 2,
-    shared: {
-      files: [ 'kraken.json', '.env' ]
-    },
-    pm2: {
-      json: path.join(shipit.config.deployTo, 'current', 'pm2.json')
-    }
-  },
-  npm: {
-    remote: true
-  },
-  vagrant: {
-    servers: 'deploy@127.0.0.1:2222'
-  }
-};
+var path = require('path');
 
 module.exports = function(shipit) {
   require('shipit-deploy')(shipit);
   require('shipit-npm')(shipit);
   require('shipit-shared')(shipit);
   require('shipit-pm2')(shipit);
+
+
+  var options = {
+    default: {
+      workspace: '/tmp/kraken',
+      deployTo: '/apps/kraken',
+      branch: 'v2',
+      repositoryUrl: 'git@github.com:subvisual/kraken.git',
+      keepReleases: 2,
+      shared: {
+        files: [ 'kraken.json', '.env' ]
+      },
+      pm2: {
+        json: '/apps/kraken/current/pm2.json'
+      }
+    },
+    npm: {
+      remote: true
+    },
+    vagrant: {
+      servers: 'deploy@127.0.0.1:2222'
+    }
+  };
 
   shipit.initConfig(options);
 
