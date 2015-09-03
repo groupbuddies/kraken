33bd1f9ce74bb1b4e6e3fa9bafa3f127a064b7da
Gabriel Poça <gabrielpoca@gmail.com>
2015-09-03 15:19:18 +0100

    shipit-shared and shipit-pm2 integration

--- deploy/vagrant/manifest.pp
+++ deploy/vagrant/manifest.pp
@@ -1,3 +1,8 @@
 class { 'nodejs':
   repo_url_suffix => 'node_0.10',
 }
+
+package { 'pm2':
+  ensure   => 'present',
+  provider => 'npm',
+}
--- shipitfile.js
+++ shipitfile.js
@@ -5,6 +5,9 @@ var options = {
     branch: 'v2',
     repositoryUrl: 'git@github.com:subvisual/kraken.git',
     keepReleases: 2,
+    shared: {
+      files: [ 'kraken.json', '.env' ]
+    }
   },
   npm: {
     remote: true
@@ -17,6 +20,8 @@ var options = {
 module.exports = function(shipit) {
   require('shipit-deploy')(shipit);
   require('shipit-npm')(shipit);
+  require('shipit-shared')(shipit);
+  require('shipit-pm2')(shipit);
 
   shipit.initConfig(options);
 
