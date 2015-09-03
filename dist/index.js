0686d1ad67352f9b21750356e8b0cfe2b6f8009f
Gabriel Poça <gabrielpoca@gmail.com>
2015-09-03 13:57:15 +0100

    Add google module (not tested because user limit in google apps)

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,7 @@ require('dotenv').load();
 
 var vantage = require('vantage')();
 
+var key = require('../' + process.env.GOOGLE_KEY_FILENAME);
 var _process$env = process.env;
 var GITHUB_USERNAME = _process$env.GITHUB_USERNAME;
 var GITHUB_PASSWORD = _process$env.GITHUB_PASSWORD;
@@ -13,6 +14,8 @@ var github = require('modules/github')({
   password: GITHUB_PASSWORD
 });
 
+var google = require('modules/google')(key);
+
 vantage.command('apprenticeship members add <username>').action(function (params) {
   var _this = this;
 
@@ -21,6 +24,16 @@ vantage.command('apprenticeship members add <username>').action(function (params
   })['catch'](function (err) {
     _this.log(err.message);
     _this.log('Please make sure you have the correct credentials on the .env file');
+  });
+});
+
+vantage.command('google members add <name> <password>').action(function () {
+  var _this2 = this;
+
+  google.users.add().then(function () {
+    return _this2.log('Complete');
+  })['catch'](function (err) {
+    return console.log(err, err.stack);
   });
 });
 
---
+++ dist/modules/google/index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var Q = require('Q');
+var google = require('googleapis');
+
+var scopes = ['https://www.googleapis.com/auth/admin.directory.group', 'https://www.googleapis.com/auth/admin.directory.user'];
+
+function authorize(client) {
+  var deferred = Q.defer();
+  client.authorize(deferred.makeNodeResolver());
+  return deferred.promise;
+}
+
+function usersAdd(client, name, password) {
+  return authorize(client).then(function (tokens) {
+    var admin = google.admin('directory_v1');
+
+    var user = {
+      resource: {
+        name: {
+          familyName: name,
+          givenName: 'Snow'
+        },
+        password: password,
+        primaryEmail: name + '@subvisual.co'
+      }
+    };
+
+    admin.users.insert(user, function (err) {
+      console.log(err, err.stack);
+    });
+  });
+}
+
+module.exports = function (key) {
+  var client_email = key.client_email;
+  var private_key = key.private_key;
+
+  var jwtClient = new google.auth.JWT(client_email, null, private_key, scopes, 'gabriel@subvisual.co');
+
+  return {
+    users: {
+      add: function add(name, password) {
+        return usersAdd(jwtClient, name, password);
+      }
+    }
+  };
+};
